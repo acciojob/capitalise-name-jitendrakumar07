dda13cb1ffbfa1a295a252eafcd6a5486eb52494
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-13 12:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.getElementById("fname").onblur = function() {
+let  input = document.getElementById("fname").onblur = function() {
   this.value = this.value.toUpperCase();
 };
